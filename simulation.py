9b04163f627c0c813c5f497435498cf6126e4c59
lotlouischoitslab <louis.s.cho@gmail.com>
2023-01-15 21:20:09 -0600

    add

---
+++ simulation.py
@@ -0,0 +1,35 @@
+import random
+import time
+import threading
+import pygame
+import sys
+
+# Default values of signal timers in seconds
+defaultGreen = {0:20, 1:20, 2:20, 3:20}
+defaultRed = 150
+defaultYellow = 5
+signals = []
+noOfSignals = 4
+currentGreen = 0   # Indicates which signal is green currently
+nextGreen = (currentGreen+1)%noOfSignals 
+currentYellow = 0   # Indicates whether yellow signal is on or off
+speeds = {'car':2.25, 'bus':1.8, 'truck':1.8, 'bike':2.5}  # average speeds of vehicles
+
+# Coordinates of vehicles’ start
+x = {'right':[0,0,0], 'down':[755,727,697], 'left':[1400,1400,1400], 'up':[602,627,657]}
+y = {'right':[348,370,398], 'down':[0,0,0], 'left':[498,466,436], 'up':[800,800,800]}
+vehicles = {'right': {0:[], 1:[], 2:[], 'crossed':0}, 'down': {0:[], 1:[], 2:[], 'crossed':0}, 'left': {0:[], 1:[], 2:[], 'crossed':0}, 'up': {0:[], 1:[], 2:[], 'crossed':0}}
+vehicleTypes = {0:'car', 1:'bus', 2:'truck', 3:'bike'}
+directionNumbers = {0:'right', 1:'down', 2:'left', 3:'up'}
+
+# Coordinates of signal image, timer, and vehicle count
+signalCoods = [(530,230),(810,230),(810,570),(530,570)]
+signalTimerCoods = [(530,210),(810,210),(810,550),(530,550)]
+
+# Coordinates of stop lines
+stopLines = {'right': 590, 'down': 330, 'left': 800, 'up': 535}
+defaultStop = {'right': 580, 'down': 320, 'left': 810, 'up': 545}
+
+# Gap between vehicles
+stoppingGap = 15    # stopping gap
+movingGap = 15   # moving gap
